20e5f992e234f89c64dca73d4dcffed56c5c0454
kangyoungmin <kangym855@gmail.com>
2023-09-26 12:51:36 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './styles/GlobalStyle'
+import theme from './styles/tehme'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div>App</div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/styles/tehme.tsx
@@ -0,0 +1,31 @@
+export type ThemeType = {
+    colors:{
+        primary:{
+            main: string,
+            dark: string,
+            light: string
+        },
+        secondary:{
+            main: string,
+            dark: string,
+            light: string
+        }
+    }
+}
+
+const theme: ThemeType = {
+    colors: {
+        primary: {
+            main: "#E7816B",
+            dark: "#1D1C1E",
+            light: "FFFFFF"
+        }, 
+        secondary : {
+            main: "#FFAD9B",
+            dark: "#333136",
+            light: "#F1F3F5"
+        }
+    }
+};
+
+export default theme;
